dfd1dd4ad79cc6f5d750ad5e48261b4164da0f38
vinmash <vinmash@mail.ru>
2023-12-08 19:02:30 +0500

    1

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -6,31 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import ru.skypro.homework.dto.RoleDTO;
-
 import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class WebSecurityConfig {
-    @Value("${spring.datasource.url}")
-    private String jdbcUrl;
-    @Value("${spring.datasource.username}")
-    private String jdbcUsername;
-
-    @Value("${spring.datasource.password}")
-    private String jdbcPassword;
-    @Value("${spring.datasource.driver-class-name}")
-    private String jdbcDriver;
 
     private static final String[] AUTH_WHITELIST = {
             "/swagger-resources/**",
@@ -41,34 +25,6 @@ public class WebSecurityConfig {
             "/register"
     };
 
-    //    @Bean
-//    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-//        UserDetails user =
-//                User.builder()
-//                        .username("user@gmail.com")
-//                        .password("password")
-//                        .passwordEncoder(passwordEncoder::encode)
-//                        .roles(RoleDTO.USER.name())
-//                        .build();
-//        return new InMemoryUserDetailsManager(user);
-//    }
-    @Bean
-    DataSource dataSource() {
-        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url(jdbcUrl);
-        dataSourceBuilder.username(jdbcUsername);
-        dataSourceBuilder.password(jdbcPassword);
-        dataSourceBuilder.driverClassName(jdbcDriver);
-        return dataSourceBuilder.build();
-    }
-
-/*
-    @Bean
-    UserDetailsManager jdbcUserDetailsManager() {
-        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource());
-        return users;
-    }
-*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
